b78583821c785294534f40074dabf0aefbd14172
m newbound <martin.nz.dev@gmail.com>
2024-06-28 15:41:47 +1200

    refactor for better variable names

--- src/GameState.c
+++ src/GameState.c
@@ -3,21 +3,30 @@
 #include <string.h>
 
 
-struct EnPassantTargetSquare {
-    bool exists;
+struct en_passant_target_square {
     uint64_t square;
+    bool exists;
 };
 
 
-struct GameState {
+struct state {
     uint64_t bitboards[2][6];   // 2 colors, 6 piece types
     bool castlingRights[4];     // 4 castling rights
    
-    struct EnPassantTargetSquare enPassantTargetSquare;
+    struct en_passant_target_square enPassantTargetSquare;
 };
 
 
-void copy_game_state(const game_state_t *fromState, game_state_t *toState) {
+void reset_state(state_t *state) {
+    memset(state->bitboards,        0,      sizeof(state->bitboards)     );    
+    memset(state->castlingRights,   true,   sizeof(state->castlingRights));
+    
+    state->enPassantTargetSquare.exists = false;
+    state->enPassantTargetSquare.square = 0;
+}
+
+
+void copy_state(const state_t *fromState, state_t *toState) {
     memcpy(toState->bitboards,      fromState->bitboards,       sizeof(toState->bitboards)      );
     memcpy(toState->castlingRights, fromState->castlingRights,  sizeof(toState->castlingRights) );
     
@@ -25,24 +34,24 @@ void copy_game_state(const game_state_t *fromState, game_state_t *toState) {
 }
 
 
-uint64_t get_color_bitboard(const game_state_t *state, color_key_t color) {
+uint64_t states_color_bitboard(const state_t *state, color_t color) {
     uint64_t colorBitboard = 0;
-    for (int i = PIECE_PAWN; i <= PIECE_KING; i++) {
+    for (size_t i = PIECE_PAWN; i <= PIECE_KING; i++) {
         colorBitboard |= state->bitboards[color][i];
     }
     return colorBitboard;
 }
 
-uint64_t get_piece_bitboard(const game_state_t *state, piece_key_t piece, color_key_t color) {
+uint64_t states_piece_bitboard(const state_t *state, piece_t piece, color_t color) {
     return state->bitboards[color][piece];
 }
 
 
-void set_piece_bitboard(game_state_t *state, piece_key_t piece, color_key_t color, uint64_t bitboard) {
+void set_states_piece_bitboard(state_t *state, piece_t piece, color_t color, uint64_t bitboard) {
     state->bitboards[color][piece] = bitboard;
 }
 
 
-bool get_castling_right(const game_state_t *state, castle_key_t castle) {
+bool state_can_castle(const state_t *state, castle_t castle) {
     return state->castlingRights[castle];
 }
--- src/GameState.h
+++ src/GameState.h
@@ -6,7 +6,8 @@
 typedef enum {
     WHITE           = 0,
     BLACK           = 1
-} color_key_t;
+} color_t;
+
 
 typedef enum {
     PIECE_PAWN      = 0,
@@ -15,23 +16,32 @@ typedef enum {
     PIECE_ROOK      = 3,
     PIECE_QUEEN     = 4,
     PIECE_KING      = 5
-} piece_key_t;
+} piece_t;
+
 
 typedef enum {
     CASTLE_KINGSIDE_WHITE   = 0,
     CASTLE_QUEENSIDE_WHITE  = 1,
     CASTLE_KINGSIDE_BLACK   = 2,
     CASTLE_QUEENSIDE_BLACK  = 3
-} castle_key_t;
+} castle_t;
 
 
 /**
- * @brief Forward declaration of the GameState struct
+ * @brief Forward declaration of the state struct
  * 
- * The inner workings of the GameState struct are hidden as
+ * The inner workings of the state struct are hidden as
  * they are not relevant to the public interface of this module
  */
-typedef struct GameState game_state_t;
+typedef struct state state_t;
+
+
+/**
+ * @brief Initializes the game state with an empty board.
+ *
+ * @param state     Pointer to the game state.
+ */
+void reset_state(state_t *state);
 
 
 /**
@@ -41,10 +51,10 @@ typedef struct GameState game_state_t;
  * when you want to make changes to the game state, but also need to keep
  * the original state unchanged for comparison or rollback purposes.
  *
- * @param fromState Pointer to the source game state.
- * @param toState Pointer to the destination game state.
+ * @param fromState     Pointer to the source game state.
+ * @param toState       Pointer to the destination game state.
  */
-void copy_game_state(const game_state_t *fromState, game_state_t *toState);
+void copy_state(const state_t *fromState, state_t *toState);
 
 
 /**
@@ -57,11 +67,12 @@ void copy_game_state(const game_state_t *fromState, game_state_t *toState);
  * This function is needed to check if a player is allowed to castle at a given point in the game.
  * Castling rights can be lost if the king or the rook has moved, or if the king is in check.
  *
- * @param state Pointer to the game state.
- * @param castle The type of castling to check the right for.
- * @return true if the player has the right to castle, false otherwise.
+ * @param state     Pointer to the game state.
+ * @param castle    The type of castling to check the right for.
+ * 
+ * @return          true if the player has the right to castle, false otherwise.
  */
-bool get_castling_right(const game_state_t *state, castle_key_t castle);
+bool state_can_castle(const state_t *state, castle_t castle);
 
 
 /**
@@ -70,11 +81,12 @@ bool get_castling_right(const game_state_t *state, castle_key_t castle);
  * A bitboard is a data structure used in computer systems that play board games, where each bit corresponds
  * to a game board space or piece. This function retrieves the bitboard for a specific color.
  *
- * @param state Pointer to the game state.
- * @param color The color of the pieces to get the bitboard for.
- * @return The bitboard for the specified color.
+ * @param state     Pointer to the game state.
+ * @param color     The color of the pieces to get the bitboard for.
+ * 
+ * @return          The bitboard for the specified color.
  */
-uint64_t get_color_bitboard(const game_state_t *state, color_key_t color);
+uint64_t states_color_bitboard(const state_t *state, color_t color);
 
 
 /**
@@ -82,12 +94,13 @@ uint64_t get_color_bitboard(const game_state_t *state, color_key_t color);
  *
  * This function retrieves the bitboard for a specific type of piece of a specific color.
  *
- * @param state Pointer to the game state.
- * @param piece The type of piece to get the bitboard for.
- * @param color The color of the pieces to get the bitboard for.
- * @return The bitboard for the specified piece and color.
+ * @param state     Pointer to the game state.
+ * @param piece     The type of piece to get the bitboard for.
+ * @param color     The color of the pieces to get the bitboard for.
+ * 
+ * @return          The bitboard for the specified piece and color.
  */
-uint64_t get_piece_bitboard(const game_state_t *state, piece_key_t piece, color_key_t color);
+uint64_t states_piece_bitboard(const state_t *state, piece_t piece, color_t color);
 
 
 /**
@@ -95,12 +108,12 @@ uint64_t get_piece_bitboard(const game_state_t *state, piece_key_t piece, color_
  *
  * This function is used to update the game state when a piece is moved or removed from the game.
  *
- * @param state Pointer to the game state.
- * @param piece The type of piece to set the bitboard for.
- * @param color The color of the pieces to set the bitboard for.
- * @param bitboard The new bitboard for the specified piece and color.
+ * @param state         Pointer to the game state.
+ * @param piece         The type of piece to set the bitboard for.
+ * @param color         The color of the pieces to set the bitboard for.
+ * @param bitboard      The new bitboard for the specified piece and color.
  */
-void set_piece_bitboard(game_state_t *state, piece_key_t piece, color_key_t color, uint64_t bitboard);
+void set_states_piece_bitboard(state_t *state, piece_t piece, color_t color, uint64_t bitboard);
 
 
 #endif // GAME_STATE_H
